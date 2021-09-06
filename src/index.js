02eb69ccf093b0b2a14ff6885345393efafab350
Iryna-Grabovska <mrsirynagrabovska@gmail.com>
2021-09-06 23:42:02 +0200

    add btn

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ const colors = [
   '#009688',
   '#795548',
 ];
+const startSwitchColor = document.querySelector('[data-action="start"]')
+console.log(startSwitchColor);
+const stopSwitchColor = document.querySelector('[data-action="stop"]')
+
+console.log(stopSwitchColor);
+colorStart.addEventListener('click', onStart);
+colorStop.addEventListener('click', onStop);
+
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
