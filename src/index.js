5b18390568f83180c498cf09c53b6e0f0af4f01c
Iryna-Grabovska <mrsirynagrabovska@gmail.com>
2021-09-08 21:48:21 +0200

    work

--- src/index.js
+++ src/index.js
@@ -6,15 +6,35 @@ const colors = [
   '#009688',
   '#795548',
 ];
-const startSwitchColor = document.querySelector('[data-action="start"]')
-console.log(startSwitchColor);
-const stopSwitchColor = document.querySelector('[data-action="stop"]')
 
-console.log(stopSwitchColor);
-colorStart.addEventListener('click', onStart);
-colorStop.addEventListener('click', onStop);
+const refs = {
+  startSwitchColor: document.querySelector('[data-action="start"]'),
+  stopSwitchColor: document.querySelector('[data-action="stop"]'),
 
-
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
 };
+//let timerId = null;
+//let isActive = false;
+// const randomIntegerFromInterval = (min, max) => {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// };
+refs.startSwitchColor.addEventListener('click', () => {
+  
+
+  // if (isActive) {
+  //   return;
+  // };
+    
+  timerId = setInterval(() => {
+    const randomColor = colors[Math.floor(Math.random )* colors.length];
+       document.body.style.backgroundColor = randomColor;
+
+   }, 1000);
+  // //randomIntegerFromInterval();
+});
+
+refs.stopSwitchColor.addEventListener('click', () => {
+  clearInterval(timerId);
+  //isActive = false;
+});
+
+
